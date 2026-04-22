238b562658262c594f610cf0cc25501bfc13ddfb
s3rlyy <suksessela74@gmail.com>
2026-04-22 13:43:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    // --- FITUR NAVBAR SCROLL ---
     const navbar = document.querySelector(".navbar");
 
     window.addEventListener("scroll", function() {
-        // Jika layar di-scroll lebih dari 50 pixel ke bawah
         if (window.scrollY > 50) {
             navbar.classList.add("scrolled");
         } else {
-            // Jika kembali ke paling atas
             navbar.classList.remove("scrolled");
         }
     });
